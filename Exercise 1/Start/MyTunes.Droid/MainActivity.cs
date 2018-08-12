666dbc026d3288a9f618f985dc052cf3f394f054
prof79 <markus@markusegger.at>
2018-08-12 22:20:28 +0200

    My solution for example 1.

--- Exercise 1/Start/MyTunes.Droid/MainActivity.cs
+++ Exercise 1/Start/MyTunes.Droid/MainActivity.cs
@@ -1,4 +1,5 @@
-﻿using Android.App;
+﻿using System.Linq;
+using Android.App;
 using Android.OS;
 
 namespace MyTunes
@@ -6,15 +7,26 @@ namespace MyTunes
 	[Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : ListActivity
 	{
-		protected override void OnCreate(Bundle bundle)
+		protected override async void OnCreate(Bundle bundle)
 		{
 			base.OnCreate(bundle);
 
-			ListAdapter = new ListAdapter<string>() {
-				DataSource = new[] { "One", "Two", "Three" }
-			};
-		}
+            //ListAdapter = new ListAdapter<string>() {
+            //	DataSource = new[] { "One", "Two", "Three" }
+            //};
+
+            var songs = await SongLoader.Load();
+
+            var songList = songs.ToList();
+
+            var songListAdapter = new ListAdapter<Song>()
+            {
+                DataSource = songList,
+                TextProc = (s) => s.Name,
+                DetailTextProc = (s) => $"{s.Artist} - {s.Album}",
+            };
+
+            ListAdapter = songListAdapter;
+        }
 	}
 }
-
-
