cbda45f42ab45f393ce2ce7445a3d0771c8eab31
prof79 <markus@markusegger.at>
2018-08-12 23:56:18 +0200

    My solution for exercise 3.

---
+++ Exercise 3/Start/MyTunes.NetStandard/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Http;
+
+namespace MyTunes
+{
+    public static class StringExtensions
+    {
+        public static string RuinSongName(this string name)
+            => name.Replace("Crocodile", "Alligator");
+
+        public static HttpClient HttpClient;
+    }
+}
--- Exercise 3/Start/MyTunes.Shared/SongLoader.cs
+++ Exercise 3/Start/MyTunes.Shared/SongLoader.cs
@@ -16,7 +16,15 @@ namespace MyTunes
         {
             using (var reader = new StreamReader(OpenData()))
             {
-                return JsonConvert.DeserializeObject<List<Song>>(await reader.ReadToEndAsync());
+                var songs =
+                    JsonConvert.DeserializeObject<List<Song>>(await reader.ReadToEndAsync());
+
+                foreach (var song in songs)
+                {
+                    song.Name = song.Name.RuinSongName();
+                }
+
+                return songs;
             }
         }
 
